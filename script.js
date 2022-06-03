fb86003c151300d9c00e1f53f90681d39a91ef21
Arianna Telesca <arianna.tlsc@gmail.com>
2022-06-03 14:05:59 +0200

    change script

--- script.js
+++ script.js
@@ -3,12 +3,10 @@
     var clicker = document.getElementById("clicker");
     var counter = 0;
     var puntos = document.getElementById("totalText");
-    var click = document.getElementById("clickText");
-    
+        
 
     clicker.addEventListener("click", ()=>{
         counter ++;
-        click.innerHTML = "click: "+counter+" | click/sec: 0"
         puntos.innerHTML = "Puntos: "+counter;
     });
    
